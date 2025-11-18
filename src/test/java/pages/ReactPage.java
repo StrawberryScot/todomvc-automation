39ad3ec31826d77b1c24ccc5fb75d833dd20f1bc
Benjamin Loveday <benjaminfloveday@hotmail.co.uk>
2025-11-18 15:19:47 +0000

    feat: created initial list of locators

--- src/test/java/pages/ReactPage.java
+++ src/test/java/pages/ReactPage.java
@@ -1,4 +1,29 @@
 package pages;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
 public class ReactPage {
+    protected WebDriver driver;
+
+    // locators
+    private By buttonDeleteBy = By.cssSelector("delete_button");
+    private By inputFieldBy = By.cssSelector("input_field");
+    private By editFieldBy = By.cssSelector("edit_field");
+    private By listTodoItemsBy = By.cssSelector("todo_items_list");
+    private By toggleCheckboxBy = By.cssSelector("toggle");
+    private By toggleSelectAllBy = By.cssSelector("chevron");
+    private By buttonFilterAll = By.cssSelector("all_button");
+    private By buttonFilterActive = By.cssSelector("active_button");
+    private By buttonFilterCompleted = By.cssSelector("completed_button");
+    private By buttonClearCompleted = By.cssSelector("clear_completed_button");
+    private By headingItemCounter = By.cssSelector("item_counter");
+
+    // methods
+    public void deleteItem() {
+        WebElement deleteButton = driver.findElement(deleteButtonBy);
+        deleteButton.click();
+    }
+
 }
