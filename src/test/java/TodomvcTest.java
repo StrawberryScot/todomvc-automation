73e02928db3513f8b62781d42fa86d7c914b7bae
Benjamin Loveday <benjaminfloveday@hotmail.co.uk>
2025-11-18 13:25:17 +0000

    setup: added TODO items to test class

--- src/test/java/TodomvcTest.java
+++ src/test/java/TodomvcTest.java
@@ -1,2 +1,22 @@
 public class TodomvcTest {
+    // To automate now
+    // TODO: TEST 1 Can't add an item with an empty value
+    // TODO: TEST 2 Can add a value with a single character
+    // TODO: TEST 3 Can delete a single item
+    // TODO: TEST 6 Can modify existing todo item
+    // TODO: TEST 7 Pressing Escape during item modification cancels modification
+    // TODO: TEST 8 Can add another todo item to list
+    // TODO: TEST 15 Can clear complete todo items when >0 completed todo items are listed
+    // TODO: Status bar always displays a count of remaining todo items left to do
+
+
+    // To automate later
+    // TODO: TEST 4 Can add characters and symbols
+    // TODO: TEST 5 Can add emoji characters
+    // TODO: TEST 9 Can reorder items
+    // TODO: TEST 11.1 Can filter incompleted items
+    // TODO: TEST 11.2 Can filter completed items
+    // TODO: TEST 11.3 Can filter by all items
+    // TODO: TEST 13 Can mark all todo items as complete
+    // TODO: TEST 14 Can mark all todo items as incomplete
 }
