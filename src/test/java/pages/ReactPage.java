6bc33075cd960647bf62ac83a2403f78b1245282
chibsdev <chibsdev@gmail.com>
2025-11-18 16:29:03 +0000

    feat: first seven basic action helper methods completed. listTodoItems method currently incomplete

--- src/test/java/pages/ReactPage.java
+++ src/test/java/pages/ReactPage.java
@@ -1,8 +1,11 @@
 package pages;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+
+import java.util.List;
 
 public class ReactPage {
     protected WebDriver driver;
@@ -19,19 +22,66 @@ public class ReactPage {
     private By editItemBy = By.cssSelector(".input-container:nth-child(1) > #todo-input");
     // above is index specific. MUST CHANGE
 
-    private By inputFieldBy = By.id("todo-input");
-    private By listTodoItemsBy = By.className("todo-list");
-    private By toggleSelectAllBy = By.id("toggle-all");
-    private By buttonFilterAll = By.linkText("All");
-    private By buttonFilterActive = By.linkText("Active");
-    private By buttonFilterCompleted = By.linkText("Completed");
-    private By buttonClearCompleted = By.cssSelector(".clear-completed");
-    private By headingItemCounter = By.className("todo-count");
+    private final By inputFieldBy = By.id("todo-input");
+    private final By listTodoItemsBy = By.className("todo-list");
+    private final By toggleSelectAllBy = By.id("toggle-all");
+    private final By buttonFilterAllBy = By.linkText("All");
+    private final By buttonFilterActiveBy = By.linkText("Active");
+    private final By buttonFilterCompletedBy = By.linkText("Completed");
+    private final By buttonClearCompletedBy = By.cssSelector(".clear-completed");
+    private final By headingItemCounterBy = By.className("todo-count");
 
     // methods
     public void deleteItem() {
         WebElement deleteButton = driver.findElement(deleteButtonBy);
         deleteButton.click();
     }
+
+    public String fetchItemCountText() {
+        WebElement headingItemCounter = driver.findElement(headingItemCounterBy);
+        return headingItemCounter.getText();
+    }
+
+    public void clickClearButton() {
+        WebElement buttonClearCompleted = driver.findElement(buttonClearCompletedBy);
+        buttonClearCompleted.click();
+    }
+
+
+    public void clickFilterCompletedButton() {
+        WebElement buttonFilterCompleted = driver.findElement(buttonFilterCompletedBy);
+        buttonFilterCompleted.click();
+    }
+
+    public void clickFilterActiveButton() {
+        WebElement buttonFilterActive = driver.findElement(buttonFilterActiveBy);
+        buttonFilterActive.click();
+    }
+
+    public void clickFilterAllButton() {
+        WebElement buttonFilterAll = driver.findElement(buttonFilterAllBy);
+        buttonFilterAll.click();
+    }
+
+    public void toggleAll() {
+        WebElement toggleSelectAll = driver.findElement(toggleSelectAllBy);
+        toggleSelectAll.click();
+    }
+
+    public List<String> listTodoItems() {
+        WebElement listTodoItems = driver.findElement(listTodoItemsBy);
+        listTodoItems.
+
+    }
+
+
+    public void createNewTodo(String newTodo) {
+        WebElement inputField = driver.findElement(inputFieldBy);
+        inputField.sendKeys(newTodo);
+        inputField.sendKeys(Keys.ENTER);
+    }
+
+
+
 
 }
